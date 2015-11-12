050c1ba906a586f85d00576d6af13e25f224882b
Rafael Laurindo <home-labs@outlook.com>
2015-11-12 04:06:35 -0200

    message

---
+++ lib/databases_migrations_manager.rb
@@ -0,0 +1,5 @@
+require "databases_migrations_manager/version"
+
+module DatabasesMigrationsManager
+  # Your code goes here...
+end
---
+++ lib/databases_migrations_manager/version.rb
@@ -0,0 +1,3 @@
+module DatabasesMigrationsManager
+  VERSION = "0.0.1"
+end
