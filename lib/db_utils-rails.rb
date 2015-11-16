3596281cbbe797aa81ec8db2edabb95df72165ab
Rafael Laurindo <home-labs@outllook.com>
2015-11-16 18:18:22 -0200

    modifica para carregar todas as tasks

--- lib/db_utils-rails.rb
+++ lib/db_utils-rails.rb
@@ -5,20 +5,17 @@ require "byebug"
 module DBUtils
 
   ROOT_PATH = File.expand_path "../../", __FILE__
-
-  # Com a classe Railtie não funcionou
-  # ver questão do initialize
-  # Application.rake_tasks do
-  # não se pode dar require em um arquivo que não é ruby ou que faz parte de biblioteca padrão do sistema, como .so ou .dll
-
   # os :: antes informa que não é para encapsular ao modulo DBUtils
   module ::Rails
     class Application
       rake_tasks do
-        # mapear tasks em diretórios e subdiretórios e dar load em todas
-        load File.join(ROOT_PATH, "/lib/tasks/active_record/railties/databases.rake")
+        Dir[File.join(ROOT_PATH, "/lib/tasks/", "**/*.rake")].each do |file|
+          load file
+        end
       end
     end
   end
+
+  # ver questão do initialize
 
 end
