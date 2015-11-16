16920e36e62dc3d214da292619dba6edb490b17e
Rafael Laurindo <home-labs@outllook.com>
2015-11-16 18:57:42 -0200

    adiciona suporte a initialize

--- lib/db_utils-rails.rb
+++ lib/db_utils-rails.rb
@@ -4,7 +4,10 @@ require "byebug"
 
 module DBUtils
 
+  include Rails
+
   ROOT_PATH = File.expand_path "../../", __FILE__
+
   # os :: antes informa que não é para encapsular ao modulo DBUtils
   module ::Rails
     class Application
@@ -13,6 +16,15 @@ module DBUtils
           load file
         end
       end
+    end
+  end
+
+
+  class Railtie < ::Rails::Railtie
+    config.after_initialize do
+      p "bla"
+      # ActiveSupport.on_load :active_record do
+      # end
     end
   end
 
