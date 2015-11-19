b212070ee16661e3a9088fe0352b5a8e80e51803
Rafael Laurindo <home-labs@outllook.com>
2015-11-19 05:04:56 -0200

    adiciona monkey patch

--- lib/support/monkey_patches/rake/lib/rake/dsl_definition.rb
+++ lib/support/monkey_patches/rake/lib/rake/dsl_definition.rb
@@ -1,6 +1,7 @@
 module Rake
   module DSL
 
+    private
     def file(*args, &block) # :doc:
       $stdout.puts "file method. Called of: #{caller[0]}"
       Rake::FileTask.define_task(*args, &block)
