57e45593d780843567505f50c3ce15829d6304c3
Rafael Laurindo <home-labs@outllook.com>
2016-01-03 18:21:55 -0200

    comenta monkey patch commitado em rails

--- lib/db_utils/support/monkey_patches/rails/activerecord/lib/active_record/tasks/database_tasks.rb
+++ lib/db_utils/support/monkey_patches/rails/activerecord/lib/active_record/tasks/database_tasks.rb
@@ -2,49 +2,48 @@ module ActiveRecord
   module Tasks
     module DatabaseTasks
 
-      def each_current_configuration(environment)
-        environments = [environment]
-        # add test environment only if no RAILS_ENV was specified.
-        environments << 'test' if environment == 'development' && ENV['RAILS_ENV'].nil?
+      # commited
+      # def create(*arguments)
+      #   configuration = arguments.first
+      #   begin
+      #     class_for_adapter(configuration['adapter']).new(*arguments).create
+      #   rescue DatabaseAlreadyExists
+      #     $stderr.puts "#{configuration['database']} already exists"
+      #   rescue Exception => error
+      #     $stderr.puts error, *(error.backtrace)
+      #     $stderr.puts "Couldn't create database for #{configuration.inspect}"
+      #   else
+      #     $stderr.puts "Database #{configuration['database']} has been created"
+      #   end
+      # end
 
-        db_configs = ActiveRecord::Base.configurations
-        configurations = []
+      # commited
+      # private
 
-        if ENV['NAMESPACE']
-          namespaces = [ENV['NAMESPACE']]
-        else
-          namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
-        end
+      # def each_current_configuration(environment)
+      #   environments = [environment]
+      #   # add test environment only if no RAILS_ENV was specified.
+      #   environments << 'test' if environment == 'development' && ENV['RAILS_ENV'].nil?
+      #   db_configs = ActiveRecord::Base.configurations
+      #   configurations = []
 
-        namespaces_configs = db_configs.values_at(*namespaces)
+      #   if ENV['NAMESPACE']
+      #     namespaces = [ENV['NAMESPACE']]
+      #   else
+      #     namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
+      #   end
+      #   namespaces_configs = db_configs.values_at(*namespaces)
+      #   if namespaces_configs.any?
+      #     namespaces_configs.each do |namespace|
+      #       configurations << namespace.values_at(*environments)
+      #     end
+      #   end
 
-        if namespaces_configs.any?
-          namespaces_configs.each do |namespace|
-            configurations << namespace.values_at(*environments)
-          end
-        end
-
-        configurations << db_configs.values_at(*environments)
-
-        configurations.flatten.compact.each do |configuration|
-          yield configuration unless configuration['database'].blank?
-        end
-      end
-
-      def create(*arguments)
-        configuration = arguments.first
-
-        begin
-          class_for_adapter(configuration['adapter']).new(*arguments).create
-        rescue DatabaseAlreadyExists
-          $stderr.puts "#{configuration['database']} already exists"
-        rescue Exception => error
-          $stderr.puts error, *(error.backtrace)
-          $stderr.puts "Couldn't create database for #{configuration.inspect}"
-        else
-          $stderr.puts "Database #{configuration['database']} has been created"
-        end
-      end
+      #   configurations << db_configs.values_at(*environments)
+      #   configurations.flatten.compact.each do |configuration|
+      #     yield configuration unless configuration['database'].blank?
+      #   end
+      # end
 
     end
 
