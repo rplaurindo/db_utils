60278e9a890bc2da4263257a605f65ff6c974d01
Rafael Laurindo <home-labs@outllook.com>
2016-01-04 05:37:43 -0200

    resolve parte do problema. Agora falta o stablish connection por namespace

--- lib/db_utils/support/monkey_patches/rails/activerecord/lib/active_record/tasks/database_tasks.rb
+++ lib/db_utils/support/monkey_patches/rails/activerecord/lib/active_record/tasks/database_tasks.rb
@@ -2,6 +2,26 @@ module ActiveRecord
   module Tasks
     module DatabaseTasks
 
+      def migrations_paths
+        paths = Rails.application.paths['db/migrate'].to_a
+        namespaces = []
+        root_path = paths.first
+
+        if ENV['NAMESPACE']
+          namespaces = [ENV['NAMESPACE']]
+        elsif ENV['NAMESPACES']
+          namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
+        end
+
+        if namespaces.any?
+          namespaces.each do |namespace|
+            paths << "#{root_path}/#{namespace}"
+          end
+        end
+
+        @migrations_paths ||= paths
+      end
+
       # commited
       # def create(*arguments)
       #   configuration = arguments.first
@@ -17,33 +37,32 @@ module ActiveRecord
       #   end
       # end
 
-      # commited
-      # private
+      private
 
-      # def each_current_configuration(environment)
-      #   environments = [environment]
-      #   # add test environment only if no RAILS_ENV was specified.
-      #   environments << 'test' if environment == 'development' && ENV['RAILS_ENV'].nil?
-      #   db_configs = ActiveRecord::Base.configurations
-      #   configurations = []
+      def each_current_configuration(environment)
+        environments = [environment]
+        # add test environment only if no RAILS_ENV was specified.
+        environments << 'test' if environment == 'development' && ENV['RAILS_ENV'].nil?
+        db_configs = ActiveRecord::Base.configurations
+        configurations = []
 
-      #   if ENV['NAMESPACE']
-      #     namespaces = [ENV['NAMESPACE']]
-      #   else
-      #     namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
-      #   end
-      #   namespaces_configs = db_configs.values_at(*namespaces)
-      #   if namespaces_configs.any?
-      #     namespaces_configs.each do |namespace|
-      #       configurations << namespace.values_at(*environments)
-      #     end
-      #   end
+        if ENV['NAMESPACE']
+          namespaces = [ENV['NAMESPACE']]
+        else ENV['NAMESPACES']
+          namespaces = ENV['NAMESPACES'] ? ENV['NAMESPACES'].split(",") : []
+        end
+        namespaces_configs = db_configs.values_at(*namespaces)
+        if namespaces_configs.any?
+          namespaces_configs.each do |namespace|
+            configurations << namespace.values_at(*environments)
+          end
+        end
 
-      #   configurations << db_configs.values_at(*environments)
-      #   configurations.flatten.compact.each do |configuration|
-      #     yield configuration unless configuration['database'].blank?
-      #   end
-      # end
+        configurations << db_configs.values_at(*environments)
+        configurations.flatten.compact.each do |configuration|
+          yield configuration unless configuration['database'].blank?
+        end
+      end
 
     end
 
