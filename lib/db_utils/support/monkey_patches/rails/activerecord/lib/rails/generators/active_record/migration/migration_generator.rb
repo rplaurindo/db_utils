0891575754a0b5fedaf4ec9dbffb788685c5e33d
Rafael Laurindo <home-labs@outllook.com>
2016-01-13 05:09:34 -0200

    comenta generator que gera erro quando não comentado, mesmo que funcione normalmente no Rails

--- lib/db_utils/support/monkey_patches/rails/activerecord/lib/rails/generators/active_record/migration/migration_generator.rb
+++ lib/db_utils/support/monkey_patches/rails/activerecord/lib/rails/generators/active_record/migration/migration_generator.rb
@@ -1,24 +1,24 @@
-module ActiveRecord
-  module Generators # :nodoc:
-    class MigrationGenerator < Base # :nodoc:
+# module ActiveRecord
+#   module Generators # :nodoc:
+#     class MigrationGenerator < Base # :nodoc:
 
-      # changed
-      def create_migration_file
-        set_local_assigns!
-        validate_file_name!
-        migration_template @migration_template, resolve_path
-      end
+#       # changed
+#       def create_migration_file
+#         set_local_assigns!
+#         validate_file_name!
+#         migration_template @migration_template, resolve_path
+#       end
 
-      private
-        # added
-        def resolve_path
-          path = "db/migrate"
-          if ENV['RAILS_NAMESPACE']
-            path += "/#{ENV['RAILS_NAMESPACE']}"
-          end
-          "#{path}/#{file_name}.rb"
-        end
+#       private
+#         # added
+#         def resolve_path
+#           path = "db/migrate"
+#           if ENV['RAILS_NAMESPACE']
+#             path += "/#{ENV['RAILS_NAMESPACE']}"
+#           end
+#           "#{path}/#{file_name}.rb"
+#         end
 
-    end
-  end
-end
+#     end
+#   end
+# end
