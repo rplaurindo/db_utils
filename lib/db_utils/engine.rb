12765c4fce5b237ffd209aa7a44a654a9f264444
Rafael Laurindo <rplaurindo>
2016-06-24 16:55:58 -0300

    change CONSTANT to method

--- lib/db_utils/engine.rb
+++ lib/db_utils/engine.rb
@@ -1,27 +1,44 @@
-ROOT_PATH = File.expand_path "../../..", __FILE__
-LIB_PATH = File.join(ROOT_PATH, "lib")
-EXTENSIONS_PATH = File.join(ROOT_PATH, "config/initializers/extensions")
-
 module DBUtils
+
+  # works only called in another module
+  protected
+    def root_path
+      File.expand_path "../../../", __FILE__
+    end
+
+    def lib_path
+      File.join(root_path, "lib")
+    end
+
+    def extensions_path
+      File.join(root_path, "config/initializers/extensions")
+    end
+
   module Rails
+
     class Engine < ::Rails::Engine
+      extend DBUtils
+
       isolate_namespace DBUtils
 
+
       config.after_initialize do
-        Dir[File.join(LIB_PATH, "**/*.rb")].each do |file|
+        Dir[File.join(lib_path, "**/*.rb")].each do |file|
           require file
         end
       end
 
       rake_tasks do
-        Dir[File.join(EXTENSIONS_PATH, "**/*.rake")].each do |file|
+        Dir[File.join(extensions_path, "**/*.rake")].each do |file|
           load file
         end
       end
 
       class Railtie < ::Rails::Railtie
 
-        Dir[File.join(EXTENSIONS_PATH, "rails", "**/*.rb")].each do |file|
+        extend DBUtils
+
+        Dir[File.join(extensions_path, "**/*.rb")].each do |file|
           require file
         end
 
